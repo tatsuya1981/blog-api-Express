15dcc30b21970ca2010acf4d64c7725a4742296f
tatsuya <clarkmaster@icloud.com>
2024-08-01 17:14:49 +0900

    記事の削除機能実装完了

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -76,4 +76,20 @@ router.patch('/:id', authMiddleware, async (req: AuthRequest, res) => {
   }
 });
 
+router.delete('/:id', authMiddleware, async (req: AuthRequest, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: '記事が見つからないよ！' });
+    }
+    if (post.userId !== req.user?.id) {
+      return res.status(403).json({ error: '操作する権限がないよ！！' });
+    }
+    await post.destroy();
+    res.status(200).send(`記事が正常に削除されました！  ※※ 削除された記事ＩＤ：${post.id} ※※`);
+  } catch (error) {
+    res.status(500).json({ error: '記事の削除に失敗・・・' });
+  }
+});
+
 export default router;
