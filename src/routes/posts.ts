f156ab81c03f6ee1edac7d6d6563499c517fe0c8
tatsuya <clarkmaster@icloud.com>
2024-08-01 16:51:49 +0900

    記事の更新設定完了

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -55,4 +55,25 @@ router.post('/', authMiddleware, async (req: AuthRequest, res) => {
   }
 });
 
+router.patch('/:id', authMiddleware, async (req: AuthRequest, res) => {
+  try {
+    const post = await Post.findByPk(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: '探している記事は無さそうだよ！' });
+    }
+    if (post.userId !== req.user?.id) {
+      return res.status(403).json({ error: '操作する権限がないよ！！' });
+    }
+    const { title, body, status, categoryIds } = req.body.post;
+    await post.update({ title, body, status });
+    if (categoryIds) {
+      await post.$set('categories', categoryIds);
+    }
+    res.json({ post });
+  } catch (error) {
+    res.status(500).json({ error: '記事の更新に失敗・・・' });
+  }
+});
+
 export default router;
